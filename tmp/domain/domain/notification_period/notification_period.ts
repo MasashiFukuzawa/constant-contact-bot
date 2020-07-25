cd12c2d8156616b2148f91636e1dd7fc638d7e1d
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-07-26 00:19:40 +0900

    feature: implement notification_period domain layer

--- tmp/domain/domain/notification_period/notification_period.ts
+++ tmp/domain/domain/notification_period/notification_period.ts
@@ -1,3 +1,23 @@
-import { NotificationPeriodRepositoryInterface } from './notification_period_repository_interface'
+import { NotificationPeriodId } from "./value_object/notification_period_id";
+import { NotificationPeriodTerm } from "./value_object/notification_period_term";
+import { NotificationPeriodUnit } from "./value_object/notification_period_unit";
 
-export class NotificationPeriod {}
+export class NotificationPeriod {
+  constructor(
+    private id: NotificationPeriodId,
+    private term: NotificationPeriodTerm,
+    private unit: NotificationPeriodUnit
+  ) {}
+
+  getId(): NotificationPeriodId {
+    return this.id;
+  }
+
+  getTerm(): NotificationPeriodTerm {
+    return this.term;
+  }
+
+  getUnit(): NotificationPeriodUnit {
+    return this.unit;
+  }
+}
