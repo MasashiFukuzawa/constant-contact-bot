47814473a1f2aeb8642b7b592605b1981f2702e6
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-07-26 18:14:50 +0900

    style: fix lint

--- src/google_apps_script/functions/push_messages.ts
+++ src/google_apps_script/functions/push_messages.ts
@@ -2,13 +2,13 @@ import { SpreadsheetDearestRepository } from '../../spreadsheet_infrastructure/d
 import { SpreadsheetNotificationPeriodRepository } from '../../spreadsheet_infrastructure/notification_periods/spreadsheet_notification_period_repository';
 import { DearestPushPresenter } from '../../webhook_app/dearest/push/dearest_push_presenter';
 import { DearestPushInteractor } from '../../domain/application/dearest/dearest_push_interactor';
-import { DearestController } from '../../webhook_app/dearest/dearest_controller';
+import { DearestPushController } from '../../webhook_app/dearest/push/dearest_push_controller';
 
 function pushMessages(): void {
-  const dr = new SpreadsheetDearestRepository()
-  const npr = new SpreadsheetNotificationPeriodRepository()
-  const dpp = new DearestPushPresenter()
-  const dpuc = new DearestPushInteractor(dr, npr, dpp);
-  const dearestController = new DearestController(dpuc);
-  dearestController.pushMessages();
+  const dr = new SpreadsheetDearestRepository();
+  const npr = new SpreadsheetNotificationPeriodRepository();
+  const dpp = new DearestPushPresenter();
+  const dpi = new DearestPushInteractor(dr, npr, dpp);
+  const dearestPushController = new DearestPushController(dpi);
+  dearestPushController.pushMessages();
 };
