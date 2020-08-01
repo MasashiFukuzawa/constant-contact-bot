e2cac936fa7501206226a81f25436366b3819ca2
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-01 12:40:25 +0900

    refactor: add last_contacted_date params in help msg

--- src/use_case/dearest/help/dearest_help_output_data.ts
+++ src/use_case/dearest/help/dearest_help_output_data.ts
@@ -1,6 +1,6 @@
 export class DearestHelpOutputData {
   getMessage(): string {
-    return `1) create -d n:name t:type_id p:notification_period_id
+    return `1) create -d n:name t:typeId p:notification_period_id l:last_contacted_date
 
 2) update -d n:name t:type_id p:notification_period_id
 
