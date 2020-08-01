803c8fc6eac947d2dd41db9925c2f7e40567dec2
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-01 09:15:44 +0900

    fix: help command reply message

--- src/use_case/dearest/help/dearest_help_output_data.ts
+++ src/use_case/dearest/help/dearest_help_output_data.ts
@@ -1,10 +1,10 @@
 export class DearestHelpOutputData {
   getMessage(): string {
-    return `1) create -d n:{name} t:{type_id} p:{notification_period_id}
+    return `1) create -d n:name t:type_id p:notification_period_id
 
-2) update -d n:{name} t:{type_id} p:{notification_period_id}
+2) update -d n:name t:type_id p:notification_period_id
 
-3) delete -d n:{name}
+3) delete -d n:name
 
 4) help`;
   }
