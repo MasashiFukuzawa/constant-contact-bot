9c3f24b5519ce6e88fa3ba1795f9c956c15e41c5
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-01 10:45:29 +0900

    test: add create tests

---
+++ __tests__/domain/interactor/dearest_create_interactor.spec.ts
@@ -0,0 +1,53 @@
+import { DearestCreateInteractor } from "../../../src/domain/application/dearest/dearest_create_interactor";
+import { DearestCreatePresenter } from "../../../src/webhook_app/dearest/create/dearest_create_presenter";
+import { SpreadsheetDearestRepository } from "../../../src/spreadsheet_infrastructure/dearests/spreadsheet_dearest_repository";
+import { Dearest } from "../../../src/domain/domain/dearest/dearest";
+
+describe('DearestCreateInteractor', () => {
+  SpreadsheetApp.openById = jest.fn(() => ({
+    getSheetByName: jest.fn(() => ({
+      getLastRow: jest.fn(() => 4),
+      getLastColumn: jest.fn(() => 5),
+      getRange: jest.fn(() => ({
+        getValues: jest.fn(() => [
+          [1, 'Izuku Midoriya', 1, 3, new Date(2020, 1, 1)],
+          [2, 'Katsuki Bakugo', 2, 3, new Date(2020, 3, 1)],
+          [3, 'Ochako Uraraka', 3, 1, new Date(2020, 5, 1)],
+          [4, 'Shoto Todoroki', 4, 2, new Date(2020, 7, 1)],
+        ]),
+        setValues: jest.fn(),
+      })),
+    })),
+  })) as any;
+
+  PropertiesService.getScriptProperties = jest.fn(() => ({
+    getProperty: jest.fn(() => 'xxxxxxx')
+  })) as any;
+
+  UrlFetchApp.fetch = jest.fn();
+
+  jest.spyOn(SpreadsheetDearestRepository.prototype, 'getAll')
+    .mockReturnValue([
+      new Dearest(1, 'Izuku Midoriya', 1, 3, new Date(2020, 1, 1)),
+      new Dearest(2, 'Katsuki Bakugo', 2, 3, new Date(2020, 3, 1)),
+      new Dearest(3, 'Ochako Uraraka', 3, 1, new Date(2020, 5, 1)),
+      new Dearest(4, 'Shoto Todoroki', 4, 2, new Date(2020, 7, 1)),
+    ]);
+
+  const sdr = new SpreadsheetDearestRepository();
+  const dcp = new DearestCreatePresenter();
+  const dci = new DearestCreateInteractor(sdr, dcp);
+  const replyToken = 'some_reply_token';
+
+  describe('SpreadsheetInfrastructure', () => {
+    describe('#handle', () => {
+      it('sends a create message successfully', () => {
+        const name = 'All Might';
+        const typeId = 5;
+        const notificationPeriodId = 4;
+        dci.handle(replyToken, name, typeId, notificationPeriodId)
+        expect(UrlFetchApp.fetch).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
--- __tests__/infrastructure/spreadsheet/dearest_repository.spec.ts
+++ __tests__/infrastructure/spreadsheet/dearest_repository.spec.ts
@@ -22,9 +22,10 @@ describe('DearestRepository', () => {
     getProperty: jest.fn(() => 'SPREAD_SHEET_ID'),
   })) as any;
 
+  const sdr = new SpreadsheetDearestRepository();
+
   describe('#getAll', () => {
     it('returns all data in dearests table', () => {
-      const sdr = new SpreadsheetDearestRepository();
       const data = sdr.getAll();
       expect(data[0]).toStrictEqual(new Dearest(1, 'Izuku Midoriya', 1, 3, new Date(2020, 1, 1)));
       expect(data[1]).toStrictEqual(new Dearest(2, 'Katsuki Bakugo', 2, 3, new Date(2020, 3, 1)));
@@ -34,20 +35,46 @@ describe('DearestRepository', () => {
   });
 
   describe('#findByName', () => {
-    it('returns a dearest resource', () => {
-      const sdr = new SpreadsheetDearestRepository();
-      const dearest = sdr.findByName('Izuku Midoriya');
-      expect(dearest.getId().toNumber()).toBe(1);
-      expect(dearest.getName().toString()).toBe('Izuku Midoriya');
-      expect(dearest.getTypeId().toNumber()).toBe(1);
-      expect(dearest.getNotificationPeriodId().toNumber()).toBe(3);
-      expect(dearest.getLastContactedDate().toDate()).toStrictEqual(new Date(2020, 1, 1));
+    describe('when valid', () => {
+      it('returns a dearest resource', () => {
+        const dearest = sdr.findByName('Izuku Midoriya');
+        expect(dearest.getId().toNumber()).toBe(1);
+        expect(dearest.getName().toString()).toBe('Izuku Midoriya');
+        expect(dearest.getTypeId().toNumber()).toBe(1);
+        expect(dearest.getNotificationPeriodId().toNumber()).toBe(3);
+        expect(dearest.getLastContactedDate().toDate()).toStrictEqual(new Date(2020, 1, 1));
+      });
     });
 
-    it('returns null', () => {
-      const sdr = new SpreadsheetDearestRepository();
-      const dearest = sdr.findByName('All Might');
-      expect(dearest).toBe(null);
+    describe('when invalid', () => {
+      it('returns null', () => {
+        const dearest = sdr.findByName('All Might');
+        expect(dearest).toBe(null);
+      });
+    });
+  });
+
+  describe('#create', () => {
+    describe('when valid', () => {
+      it('creates successfully', () => {
+        const now = new Date('2020/7/29 01:00:00');
+        Date.now = jest.fn().mockReturnValue(now.valueOf());
+        jest.spyOn(global, 'Date').mockImplementation();
+
+        const dearest = sdr.create('All Might', 5, 4);
+        expect(dearest.getId().toNumber()).toBe(5);
+        expect(dearest.getName().toString()).toBe('All Might');
+        expect(dearest.getTypeId().toNumber()).toBe(5);
+        expect(dearest.getNotificationPeriodId().toNumber()).toBe(4);
+        expect(dearest.getLastContactedDate().toDate()).toStrictEqual(new Date());
+      });
+    });
+
+    describe('when invalid', () => {
+      it('fails to create because of unique constraint', () => {
+        const dearest = sdr.create('Izuku Midoriya', 5, 4);
+        expect(dearest).toBe(null);
+      });
     });
   });
 
@@ -57,12 +84,11 @@ describe('DearestRepository', () => {
       Date.now = jest.fn().mockReturnValue(now.valueOf());
       jest.spyOn(global, 'Date').mockImplementation();
 
-      const sdr = new SpreadsheetDearestRepository();
       const dearest = new Dearest(1, 'Izuku Midoriya', 1, 3, new Date(2020, 1, 1));
-      const result = sdr.update(dearest, 4, 4);
-      expect(result.getTypeId().toNumber()).toBe(4);
-      expect(result.getNotificationPeriodId().toNumber()).toBe(4);
-      expect(result.getLastContactedDate().toDate()).toStrictEqual(new Date());
+      const updatedDearest = sdr.update(dearest, 4, 4);
+      expect(updatedDearest.getTypeId().toNumber()).toBe(4);
+      expect(updatedDearest.getNotificationPeriodId().toNumber()).toBe(4);
+      expect(updatedDearest.getLastContactedDate().toDate()).toStrictEqual(new Date());
     });
   });
 });
