2f29fb691a411097c8c1c4fd8082345a7be3963e
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-02 12:42:02 +0900

    fix: rename google calender api client class name

--- src/external_api/client/google_calender_api_client.ts
+++ src/external_api/client/google_calender_api_client.ts
@@ -1,6 +1,6 @@
 import CalendarEvent = GoogleAppsScript.Calendar.CalendarEvent;
 
-export class GoogleCalenderClient {
+export class GoogleCalenderApiClient {
   mapAnnualEvents(): { title: string, month: number, day: number }[] {
     const events = this.getAnnualEvents();
     return events.map(e => {
@@ -8,8 +8,8 @@ export class GoogleCalenderClient {
       // NOTE: getStartTimeメソッドはカレンダーに設定されたタイムゾーンを基準に日付が取得されるため、
       // GASとカレンダーでタイムゾーン設定が同じでないと日付がずれる点に注意が必要
       const date = e.getStartTime();
-      const month = date.getMonth();
-      const day = date.getDay();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
       return { title, month, day };
     });
   }
--- src/google_apps_script/functions/migrate_birthday.ts
+++ src/google_apps_script/functions/migrate_birthday.ts
@@ -1,10 +1,10 @@
 import { BirthdayMigrateController } from "../../batch/birthday_migrate_controller";
 import { BirthdayMigrateInteractor } from "../../domain/application/batch/birthday_migrate_interactor";
 import { SpreadsheetDearestRepository } from "../../spreadsheet_infrastructure/dearests/spreadsheet_dearest_repository";
-import { GoogleCalenderClient } from "../../external_api/client/google_calender_client";
+import { GoogleCalenderApiClient } from "../../external_api/client/google_calender_api_client";
 
 function migrateBirthday(): void {
-  const calenderClient = new GoogleCalenderClient();
+  const calenderClient = new GoogleCalenderApiClient();
   const events = calenderClient.mapAnnualEvents();
 
   const dr = new SpreadsheetDearestRepository();
